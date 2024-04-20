bf6fc56838ce9c546092d5871f72df37edbd6fba
Bharat Kathi <bharat1031@gmail.com>
2024-04-19 23:12:46 -0700

    firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:battleship_lahacks/firebase_options.dart';
 import 'package:battleship_lahacks/pages/home_page.dart';
 import 'package:battleship_lahacks/utils/config.dart';
+import 'package:battleship_lahacks/utils/logger.dart';
 import 'package:battleship_lahacks/utils/theme.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:fluro/fluro.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -20,6 +24,11 @@ Future<void> main() async {
   MAPBOX_ACCESS_TOKEN = dotenv.env['MAPBOX_ACCESS_TOKEN']!;
 
   prefs = await SharedPreferences.getInstance();
+
+  log("Battleship v${appVersion.toString()} – ${appVersion.getVersionCode()}");
+  FirebaseApp app = await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  log("Initialized default app $app");
+  FirebaseAnalytics analytics = FirebaseAnalytics.instance;
 
   // ROUTE DEFINITIONS
   router.define("/", handler: Handler(handlerFunc: (BuildContext? context, Map<String, dynamic>? params) {
--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -1,3 +1,4 @@
+import 'package:battleship_lahacks/models/version.dart';
 import 'package:fluro/fluro.dart';
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
@@ -10,6 +11,9 @@ final RouteObserver<ModalRoute> routeObserver = RouteObserver<ModalRoute>();
 var httpClient = http.Client();
 
 late SharedPreferences prefs;
+
+Version appVersion = Version("1.0.0+1");
+Version stableVersion = Version("1.0.0+1");
 
 String MAPBOX_PUBLIC_TOKEN = "mapbox-public-token";
 String MAPBOX_ACCESS_TOKEN = "mapbox-access-token";
